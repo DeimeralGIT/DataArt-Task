dc0ee820537adb04977033f5b16b68de505dc939
DeimeralGIT <vahanhekimyan99@gmail.com>
2023-06-25 14:56:04 +0400

    updated random generation to work with faker

--- data_art_task/lib/utils/transaction_generator.dart
+++ data_art_task/lib/utils/transaction_generator.dart
@@ -1,16 +1,16 @@
 //creates 50 random transactions
-import 'dart:math';
+import 'package:faker/faker.dart';
 
 import '../constants/transaction_type.dart';
 import '../models/transaction_model.dart';
 
 List<TransactionModel> generateRandomTransactions() {
+  final faker = Faker();
   //creates random ids insuring there are no duplicates
   final List<int> ids = [];
 
-  final random = Random();
   while (ids.length < 50) {
-    final id = random.nextInt(8999999) + 1000000;
+    final id = faker.randomGenerator.integer(9999999, min: 1000000);
     if (!ids.contains(id)) {
       ids.add(id);
     }
@@ -21,18 +21,12 @@ List<TransactionModel> generateRandomTransactions() {
   for (int i = 0; i < 50; i++) {
     list.add(
       TransactionModel(
-        type: TransactionType.values[random.nextInt(3)],
+        type: TransactionType.values[faker.randomGenerator.integer(3)],
         id: ids[i],
-        value: (random.nextInt(9969) + 30) * random.nextDouble(),
-        date: DateTime.now()
-          ..add(
-            Duration(
-              days: random.nextInt(1000),
-              hours: random.nextInt(23),
-              minutes: random.nextInt(59),
-            ),
-          ),
-        commission: random.nextDouble() * 3,
+        value: (faker.randomGenerator.integer(9999, min: 30)) *
+            faker.randomGenerator.decimal(),
+        date: faker.date.dateTime(),
+        commission: faker.randomGenerator.decimal() * 3,
       ),
     );
   }
